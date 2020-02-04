4c184a3059bd26837a98354572f9a34a1daa7609
Skyler Ng Chung Hou <skyler.nch@gmail.com>
2020-02-04 17:45:05 +0800

    automated stock fetcher
    able to retrieve any stocks listed in the market variable, and generate CSV for each automatically

--- Technical Analysis/Driver_Code.py
+++ Technical Analysis/Driver_Code.py
@@ -1,10 +1,37 @@
 from alpha_vantage.timeseries import TimeSeries
+import os
 # API KEY------------------
-key = 'LOK9IWR45QLHXH88'
+key = "LOK9IWR45QLHXH88"
 ts = TimeSeries(key)
 #--------------------------
 
-data,meta_data = ts.get_intraday('GOOGL')
-print(data)
-for item in data:
-    print(item)
+
+# Handling Input-----------
+
+market = ["GOOGL"]
+#USA eastern time by default (GMT -5), 9:30AM opens, 4PM closes, Weekend closes
+
+for stocks in market:
+    data,meta_data = ts.get_intraday(stocks)
+    f = open("{}\\Input_CSV\\{}.txt".format(os.path.dirname(__file__),str(stocks)),'w')
+
+    for days in data:
+        print(data[days])
+        currentday = data[days]
+        
+        # Use this if viewer friendly text is needed
+        #currentline = "0. day: {}\t1. open: {}\t2. high: {}\t3. low: {}\t4. close: {}\t5. volume: {}\n"
+
+        #Use this if CSV version is needed
+        currentline = "{},{},{},{},{},{}\n"
+
+        
+        currentline = currentline.format(days,currentday["1. open"],currentday["2. high"],currentday["3. low"],currentday["4. close"],currentday["5. volume"],)
+
+        f.write(currentline)
+        
+        
+f.close()
+#--------------------------
+
+
