a182b622405ac850d7bc9bcae1ebfb676cef0ca7
Skyler Ng Chung Hou <skyler.nch@gmail.com>
2020-02-22 01:36:56 +0800

    Update Timeseries Trainer.py

--- Technical Analysis/Timeseries Trainer.py
+++ Technical Analysis/Timeseries Trainer.py
@@ -2,7 +2,18 @@ import os
 import torch
 import torch.nn as nn
 
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+
+def CreateInOutSequence(inputlist,trainingwindow):
+    sequence = []
+    for i in range(len(inputlist)-trainingwindow):
+        train_seq = inputlist[i:i+trainingwindow]
+        train_label = inputlist[i+trainingwindow:i+trainingwindow+1]
+        sequence.append((train_seq,train_label))
+    return sequence
+
 
 #Input------------------------------------
 listofitems = os.listdir("{}\\Input_CSV\\Historical".format(os.path.dirname(__file__)))
@@ -30,4 +41,40 @@ for line in rawdata:
 print("{} records found in file".format(len(rawdata)))
     
 #Preprocessing------------------------------------
-testdatasize = 0.1*len(rawdata)
+
+#percentage dictates how much of the latest records will be used as test data
+percentage = 0.1
+testdatasize = round(percentage*len(rawdata))
+
+#we will train for now, the high of the stock price
+traindata = stockdata["high"][:-testdatasize]
+testdata = stockdata["high"][-testdatasize:]
+print("training data consists of {} records, testing data consists of {} records".format(len(traindata),len(testdata)))
+
+#normalise the training data
+scaler = MinMaxScaler(feature_range=(-1,1))
+print(scaler)
+
+#convert to numpy array, then reshape to normalise
+NormalisedTrainingData = scaler.fit_transform(np.asarray(traindata).reshape(-1,1))
+
+#convert the dataset to FloatTensor object fot the pytorch model
+NormalisedTrainingData = torch.FloatTensor(NormalisedTrainingData).view(-1)
+
+#any sequence window will do, but since its daily data, its nicer to have it at 365
+trainingwindow = 365
+trainingsequence = CreateInOutSequence(NormalisedTrainingData,trainingwindow)
+
+print(trainingsequence[:5])
+
+
+
+
+
+
+
+
+
+
+
+        
